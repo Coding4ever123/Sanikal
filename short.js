303690440deef5b9ac64a0368af0504e6f80d9a3
Coding4ever123 <148124024+Coding4ever123@users.noreply.github.com>
2023-10-16 19:09:46 +0200

    Create short.js

---
+++ short.js
@@ -0,0 +1,22 @@
+const links={
+	"#FM": "sanikal-fuer-mich/",
+	"#BG": "badgalerie/",
+	"#MH": "news/sanikal-messe-hotel-2023-bozen/"
+
+}
+let hash = location.hash;
+console.log(location.hash)
+function Onreload(){
+	if (location.pathname=="/"){
+    let link = links[hash].toString();
+		if (link != "undefined"){
+			window.location.href = link;
+		  console.log(link) 
+		}
+		else if(link == "undefined"){
+			location.hash = ""
+		}
+	}
+}
+Onreload()
+
