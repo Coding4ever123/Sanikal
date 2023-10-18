10a2f682cb8208360a86b303827046e00697aefd
Coding4ever123 <148124024+Coding4ever123@users.noreply.github.com>
2023-10-18 18:33:33 +0200

    Update short.js

--- short.js
+++ short.js
@@ -1,12 +1,5 @@
 
-function httpGet(theUrl)
-{
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
-    xmlHttp.send( null );
-    return xmlHttp.responseText;
-}
-//
+
 var response = fetch("https://raw.githubusercontent.com/Coding4ever123/Sanikal/main/settings.html?token=GHSAT0AAAAAACJCNM7RBT4PYXNRSWPAC5TQZJQBDEA")
 
 var links = JSON.parse(response)
